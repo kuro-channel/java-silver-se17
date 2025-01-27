9ab9ad8833833fd5a7d608761775cae6c26180e7
takumi.kuroiwa <t.kuroiwa.0216@gmail.com>
2025-01-27 23:43:53 +0900

    20250127 commit

--- java_silver_se_17/black/chapter2/41_44/Main.java
+++ java_silver_se_17/black/chapter2/41_44/Main.java
@@ -1,86 +1,108 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // chapter2-35
-        // 配列の継承
-        A31[] a = { new C31(), null, new D31() };
-        Object[] b = a;
-        System.out.println(b[0]); // C31@1b6d3586
-        System.out.println(b[1]); // null
-        System.out.println(b[2]); // D31@4554617c
+        // chapter2-41
+        // removeメソッドは、指定した要素を削除する
+        ArrayList<Item> list = new ArrayList<>();
+        list.add(new Item("A", 100));
+        list.add(new Item("B", 200));
+        list.add(new Item("C", 300));
+        list.add(new Item("A", 100));
+        list.remove(new Item("A", 500));
 
-        // chapter2-36
-        int[][] arrayA = { { 1, 2 }, { 1, 2 }, { 1, 2, 3 } };
+        for (Item item : list) {
+            System.out.println(item.getName() + ":" + item.getPrice());
+        }
 
-        // cloneメソッドは、配列の要素をコピーする
-        int[][] arrayB = arrayA.clone();
-        int total = 0;
-        for (int[] array : arrayB) {
-            for (int i : array) {
-                total += i;
+        // chapter2-42
+        // removeメソッドは、指定したインデックスの要素を削除する
+        // かつ、後ろの要素を前に詰める
+        ArrayList<Item> list2 = new ArrayList<>();
+        list2.add(new Item("A", 100));
+        list2.add(new Item("B", 200));
+        list2.add(new Item("C", 300));
+
+        System.out.println("chapter2-42:");
+
+        // 拡張for文で要素を削除すると、カーソルがそのまま
+        // A, B, C → A, C
+        // Cは出力されない
+        for (Item item : list2) {
+            if (item.getName().equals("B")) {
+                list2.remove(item);
+            } else {
+                System.out.println(item.getName() + ":" + item.getPrice());
             }
         }
-        System.out.println("chapter2-36:" + total); // 12
 
-        // chapter2-38
-        // ジェネリクス
-        // ジェネリクスを使わないと、Object型で扱われる
-        ArrayList list = new ArrayList<>();
-        list.add("A");
-        list.add(10);
-        list.add('B');
-        for (Object obj : list) {
-            System.out.println(obj);
-        }
-
-        // ジェネリクスを使うと、指定した型で扱える
-        ArrayList<String> list2 = new ArrayList<>();
-        list2.add("A");
-        // list2.add(10); // コンパイルエラー
-        list2.add("B");
-        for (String str : list2) {
-            System.out.println(str);
-        }
-
-        // chapter2-39
-        // addメソッドは、指定したインデックスに要素を追加する
+        // chapter2-43
+        // ConcurrentModificationExceptionを見てみる
+        // 拡張for文で要素を削除すると、ConcurrentModificationExceptionが発生する
         ArrayList<String> list3 = new ArrayList<>();
         list3.add("A");
-        // java.lang.IndexOutOfBoundsException: Index: 2, Size: 1
-        // 例外が発生する
-        // list3.add(2, "B"); // 2番目に追加
+        list3.add("B");
         list3.add("C");
-        list3.add("D");
 
-        System.out.println("chapter2-39:");
-        for (String str : list3) {
+        System.out.println("chapter2-43:");
+
+        // for (String str : list3) {
+        // if (str.equals("C")) {
+        // list3.remove(str);
+        // } else {
+        // System.out.println(str);
+        // }
+        // }
+
+        // java.util.ConcurrentModificationException
+        // for (String str : list3) {
+        // System.out.println(str);
+        // }
+
+        // chapter2-44
+        // 固定長リストを作成する
+        final var list4 = List.of("A", "B", "C");
+        // list4.add("D"); // java.lang.UnsupportedOperationException
+        System.out.println("chapter2-44:");
+        for (String str : list4) {
             System.out.println(str);
         }
 
-        // chapter2-40
-        // setメソッドは、指定したインデックスの要素を置き換える
-        ArrayList<String> list4 = new ArrayList<>();
-        list4.add("A");
-        list4.set(0, "B");
-        list4.add("C");
-        list4.set(1, "D");
-
-        System.out.println("chapter2-40:");
-        for (String str : list4) {
-            System.out.print(str);
+        final var list5 = Arrays.asList(new Integer[] { 1, 2, 3 });
+        // list5.add(4); // java.lang.UnsupportedOperationException
+        System.out.println("chapter2-44:");
+        for (int i : list5) {
+            System.out.println(i);
         }
     }
 
-    static interface A31 {
-    }
+    static class Item {
+        private String name;
+        private int price;
 
-    static abstract class B31 implements A31 {
-    }
+        public Item(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
 
-    static class C31 extends B31 {
-    }
+        public boolean equals(Object obj) {
+            if (obj instanceof Item) {
+                Item item = (Item) obj;
+                if (name.equals(item.name)) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-    static class D31 extends C31 {
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
     }
 }
