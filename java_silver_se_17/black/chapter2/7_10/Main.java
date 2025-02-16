cdac7a96da59c75dab3582256495e1ef37cc66c0
takumi.kuroiwa <t.kuroiwa.0216@gmail.com>
2025-02-16 09:29:37 +0900

    20250216 commit

--- java_silver_se_17/black/chapter2/7_10/Main.java
+++ java_silver_se_17/black/chapter2/7_10/Main.java
@@ -13,6 +13,17 @@ public class Main {
         Chapter2To8C c = new Chapter2To8C();
         c.test(); // Chapter2To8C独自のメソッドが呼ばれる
 
+        var a1 = new Chapter2To8B();
+        // a1 = new Chapter2To8C(); // これはエラー、不適合な型: Chapter2To8CをChapter2To8Bに変換できません
+        // Chapter2To8BとChapter2To8Cは継承関係にないので、型推論できない
+
+        // chapter2-9
+        // String型のインスタンス生成方法について
+        String s1 = "hello"; // 文字列リテラル
+        String s2 = new String("hello"); // new演算子, Stringもクラスなのでインスタンス化できる
+        String s3 = new String(new char[] { 'h', 'e', 'l', 'l', 'o' }); // char配列
+        String s4 = String.valueOf(new char[] { 'h', 'e', 'l', 'l', 'o' }); // valueOfメソッド
+
         // chapter2-10
         String str = "hoge, world.";
         hello(str); // 参照元の値はそのまま
